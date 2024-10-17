bbdaa8407dd4a1790ea349dfd1d7fa02c33b9273
KseniaYudina <uksenia359@gmail.com>
2024-10-17 16:47:09 +0300

    delete comments

--- src/main/java/ru/netology/qa/Main.java
+++ src/main/java/ru/netology/qa/Main.java
@@ -1,7 +1,3 @@
 package ru.netology.qa;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
 }
